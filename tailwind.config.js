39642d65b5bcef4a7bf231773c330dd1e6f078f1
Tracy-maker <ydlvns@gmail.com>
2024-10-03 15:50:56 +1000

    install some packages

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{js,ts,vue}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
