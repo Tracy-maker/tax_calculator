594d2ad1e427faf415b829640a070ebcd1e5ecc6
Tracy-maker <ydlvns@gmail.com>
2024-11-04 17:59:06 +1100

    fix errors of files structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  parser: 'vue-eslint-parser', // Use vue-eslint-parser to handle Vue files
   parserOptions: {
-    parser: '@babel/eslint-parser', // Use Babel parser for JavaScript
     ecmaVersion: 2020,
     sourceType: 'module',
-    requireConfigFile: false, // Only if you don’t have a .babelrc file
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:@babel/recommended', // Enable Babel-specific linting rules
   ],
-  rules: {
-    'no-unused-vars': 'warn', // Adjust rules based on your project needs
-  },
 };
